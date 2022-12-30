c7732e7b6977198a13df17bd8a5bead160df143e
Sara <sara@studiopresent.com>
2022-12-30 08:53:41 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Worderland - Admin</title>
+</head>
+<body>
+<h1>Worderland - Admin dashboard</h1>
+
+<div>
+    <a href="#">Users</a>
+    <a href="#">Words</a>
+    <a href="#">Add a new word!</a>
+</div>
+
+</body>
+</html>
